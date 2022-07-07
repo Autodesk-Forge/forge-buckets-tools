0bb1727133fc6d1f2db3df66115d11dbe3cec02f
Adam Nagy <adam_e_nagy@hotmail.com>
2022-07-07 19:56:57 +0100

    Update start.js

--- start.js
+++ start.js
@@ -22,9 +22,6 @@ var app = require('./server/server');
 
 // start server
 var server = app.listen(app.get('port'), function () {
-  if (process.env.FORGE_CLIENT_ID == null || process.env.FORGE_CLIENT_SECRET == null)
-    console.log('*****************\nWARNING: Client ID & Client Secret not defined as environment variables.\n*****************');
-
   console.log('Starting at ' + (new Date()).toString());
   console.log('Server listening on port ' + server.address().port);
 });
